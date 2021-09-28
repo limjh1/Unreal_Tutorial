08784a99e0cf3915bea164e42a6e65828c46207b
limjh1 <jehyuk1711@gmail.com>
2021-09-28 23:01:23 +0900

    init commit
    210928

---
+++ Source/please/please.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "please.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, please, "please" );
---
+++ Source/please/pleaseGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "pleaseGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PLEASE_API ApleaseGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
